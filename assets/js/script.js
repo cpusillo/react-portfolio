6fc0bb2d0926153e0285761f06cca5b41b384bbd
cpusillo <cpusillo@gmail.com>
2020-09-22 20:58:37 -0400

    Ran fmt on all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 var submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click", function(){
+submitBtn.addEventListener("click", function () {
     alert("Thanks, I'll be in touch!");
 });
-
